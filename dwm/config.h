2203c6b15a30cfaf720c7c692fb27ae7aee9349d
Erich Schroeter <erich.schroeter@gmail.com>
2025-07-09 14:12:39 -0500

    feat: dwm change termcmd to alacritty

--- dwm/config.h
+++ dwm/config.h
@@ -9,7 +9,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 // static const char dmenufont[]       = "monospace:size=9";
 static const char *fonts[]          = { "Cascadia Code NF:Regular:size=9" };
 static const char dmenufont[]       = "Cascadia Code NF:Regular:size=9";
-static const char rtermfont[]       = "Cascadia Code NF:Regular:size=9";
+static const char termfont[]        = "Cascadia Code NF:Regular:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "rterm", "-f", rtermfont, NULL };
+static const char *termcmd[]  = { "alacritty", "--title", "term", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */

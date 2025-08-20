45c54b7730b7eb9a60a6067ba0a37e9e3cb45bbe
Erich Schroeter <erich.schroeter@gmail.com>
2025-08-20 00:35:32 -0500

    feat: space prefix battery

--- dwm/config.slstatus.h
+++ dwm/config.slstatus.h
@@ -66,7 +66,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 #ifdef WITH_BATTERY
-	{ battery_perc, "%s%%",          "BAT0" },
+	{ battery_perc, " %s%%",         "BAT0" },
 	{ datetime,     " | %s",         "%F %T" },
 #else
 	{ datetime,     " %s",         "%F %T" },

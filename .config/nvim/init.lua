7ce6affe3a99c9efb1101144b16b2bb24130d7b2
Erich Schroeter <erich.schroeter@gmail.com>
2026-04-07 19:25:05 -0500

    refactor: nvim colortheme to melange

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 require("config.lazy")
 
+vim.cmd.colorscheme 'melange'
 vim.keymap.set('n', '<C-p>', function()
   require('fzf-lua').files()
 end, { desc = 'FzfLua: Find files in current directory' })
